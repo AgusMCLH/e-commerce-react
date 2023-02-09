9c411f7fda0bd09ecd5465c8b062e64e8c33c846
m4pacheeDev <agustindiazmclh@gmail.com>
2023-02-09 09:54:41 -0300

    navBar v0.1

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './navbar.css';
+import carrito from './../../img/cartSVG.svg';
+const Navbar = () => {
+  return (
+    <>
+      <header>
+        <div className="avatar_container">
+          <img src="http://placekitten.com/300/300" alt="a" />
+        </div>
+        <ul>
+          <li>
+            <a href="#a"> Elemento #1</a>
+          </li>
+          <li>
+            <a href="#a"> Elemento #2</a>
+          </li>
+          <li>
+            <a href="#a"> Elemento #3</a>
+          </li>
+          <li>
+            <a href="#a"> Elemento #4</a>
+          </li>
+          <li>
+            <a href="#a"> Elemento #5</a>
+          </li>
+        </ul>
+        <div className="navbar_carrito-container">
+          <div className="navbar_carrito">
+            <img src={carrito} alt="Carrito sin mas" />
+            <div className="puntito"></div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
