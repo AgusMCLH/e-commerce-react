00a149bd274e35cbe999a19554998d6f9740decc
pache_dev <agustindiazmclh@gmail.com>
2023-03-10 18:42:26 -0300

    dependencias

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyA-wKh6zmVyzIclmaPMHgiv1LvMAVEptXA',
+  authDomain: 'coderhouse-d72e2.firebaseapp.com',
+  projectId: 'coderhouse-d72e2',
+  storageBucket: 'coderhouse-d72e2.appspot.com',
+  messagingSenderId: '586580024797',
+  appId: '1:586580024797:web:9cf0d3c8f1b53c276dea34',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
 
